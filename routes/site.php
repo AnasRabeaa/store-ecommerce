829c6a6e5538c5f5aa1fe642782f506e99bca0d2
Anas Rabea <anas.rabea100@gmail.com>
2022-09-03 14:17:20 +0200

    =setup front-end design

--- routes/site.php
+++ routes/site.php
@@ -12,3 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+route::get('/', function (){
+    return view('front.home');
+});
